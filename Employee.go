c220bfe219bad2acbd191d75fbd553c908896728
Shubham005.sam <shubham005.sam@gmail.com>
2023-11-07 19:03:00 +0530

    initial commit

---
+++ Employee.go
@@ -0,0 +1,41 @@
+package content
+
+import (
+	"context"
+	"log"
+	"unicode"
+
+	"cloud.google.com/go/firestore"
+)
+
+type Employee struct {
+	ID        string `firestore:"id" json:"id"`
+	FirstName string `firestore:"firstname" json:"firstname"`
+	LastName  string `firestore:"lastname" json:"lastname"`
+	Email     string `firestore:"email" json:"email"`
+	Password  string `firestore:"password" json:"password"`
+	PhoneNo   string `firestore:"phoneNo" json:"phoneNo"`
+	Role      string `firestore:"role" json:"role"`
+}
+
+func initializeFirestore() {
+	onceClient.Do(func() {
+		ctx := context.Background()
+
+		// Initialize Firestore with the service account key
+		var err error
+		client, err = firestore.NewClient(ctx, "takeoff-task-3")
+		if err != nil {
+			log.Fatalf("Failed to create Firestore client: %v", err)
+		}
+	})
+}
+func validNumberEntry(name string) bool {
+	for _, char := range name {
+		if unicode.IsLetter(char) {
+			return true
+		}
+	}
+	return false
+
+}
---
+++ createEmployee.go
@@ -0,0 +1,147 @@
+package content
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"sync"
+
+	"cloud.google.com/go/firestore"
+	"cloud.google.com/go/logging"
+	"github.com/Shubham005x1/MyValidations/validations"
+	"golang.org/x/crypto/bcrypt"
+)
+
+var (
+	client     *firestore.Client
+	logClient  *logging.Client
+	onceClient sync.Once
+)
+
+// CreateEmployee handles the creation of an employee record.
+func CreateEmployee(w http.ResponseWriter, r *http.Request) {
+	// Create a new background context.
+	ctx := context.Background()
+
+	// Initialize Firestore connection.
+	initializeFirestore()
+
+	// Initialize the Logging client for logging events.
+	logClient, _ = logging.NewClient(ctx, "takeoff-task-3")
+
+	// Ensure the logClient is closed after the function completes.
+	defer logClient.Close()
+
+	// Create a logger for this function.
+	logger := logClient.Logger("my-log")
+
+	// Log an entry indicating that the CreateEmployee function has started.
+	logger.Log(logging.Entry{
+		Payload:  "CreateEmployee function started",
+		Severity: logging.Info,
+	})
+
+	// Parse the request body to retrieve employee data.
+	var emp Employee // Assuming there's a struct named Employee.
+	err := json.NewDecoder(r.Body).Decode(&emp)
+	if err != nil {
+		http.Error(w, "Invalid request body!", http.StatusBadRequest)
+		logger.Log(logging.Entry{
+			Payload:  "Invalid request body!",
+			Severity: logging.Error,
+		})
+		return
+	}
+
+	// Check if the employee ID contains characters (non-numeric).
+	if validNumberEntry(emp.ID) {
+		http.Error(w, "Id cannot be character", http.StatusBadRequest)
+		return
+	}
+
+	// Query Firestore to check if an employee with the same ID already exists.
+	collectionRef := client.Collection("employees")
+	query := collectionRef.Where("id", "==", emp.ID)
+	data, err := query.Documents(ctx).GetAll()
+	if err != nil {
+		// Handle error querying Firestore.
+		http.Error(w, fmt.Sprintf("Error querying Firestore: %v", err), http.StatusInternalServerError)
+		return
+	}
+
+	if len(data) > 0 {
+		// At least one document was found, indicating an existing employee with the same ID.
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Employee with the same ID already exists"))
+		return
+	}
+
+	// Check if any required fields are empty.
+	if emp.ID == "" || emp.FirstName == "" || emp.LastName == "" || emp.Email == "" || emp.Password == "" || emp.PhoneNo == "" || emp.Role == "" {
+		http.Error(w, "All fields are required", http.StatusBadRequest)
+		return
+	}
+
+	// Validate FirstName and LastName to ensure they don't contain numbers.
+	if validations.ValidNameEntry(emp.FirstName) {
+		http.Error(w, "Name Cannot contain Numbers please enter valid Name", http.StatusBadRequest)
+		return
+	}
+	if validations.ValidNameEntry(emp.LastName) {
+		http.Error(w, "LastName Cannot contain Numbers please enter valid Name", http.StatusBadRequest)
+		return
+	}
+	// Assuming `emp` has a `Password` field.
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(emp.Password), bcrypt.DefaultCost)
+	if err != nil {
+		// Handle error
+		http.Error(w, "Failed to hash password", http.StatusInternalServerError)
+		return
+	}
+	emp.Password = string(hashedPassword)
+	logger.Log(logging.Entry{
+		Payload:  "Password Hashed and Saved in Database",
+		Severity: logging.Info,
+	})
+
+	// Validate the email format.
+	err = validations.IsValidEmail(emp.Email)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	err = validations.IsNumberValid(emp.PhoneNo)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// Log that the request body has been parsed successfully.
+	logger.Log(logging.Entry{
+		Payload:  "Request body parsed successfully",
+		Severity: logging.Info,
+	})
+
+	// Add the employee data to Firestore.
+	_, _, err = client.Collection("employees").Add(ctx, emp)
+	if err != nil {
+		// Handle error adding employee to Firestore.
+		http.Error(w, fmt.Sprintf("Failed to add employee: %v", err), http.StatusInternalServerError)
+		logger.Log(logging.Entry{
+			Payload:  fmt.Sprintf("Failed to add employee: %v", err),
+			Severity: logging.Error,
+		})
+		return
+	}
+
+	// Log that the employee was added successfully.
+	logger.Log(logging.Entry{
+		Payload:  "Employee added successfully",
+		Severity: logging.Info,
+	})
+
+	// Respond with a status code indicating success and a message.
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte("Employee created successfully"))
+}
